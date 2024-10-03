476b26444bba12ee4457d4081161b4929f88c5d8
boiwantlearncode <ibetank@gmail.com>
2024-10-03 17:54:19 +0800

    updated homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,27 @@
 import Link from 'next/link';
+import ShimmerButton from "@/components/ui/shimmer-button";
+import VersionTag from '../components/VersionTag';
+
+
+// Insert function that pulls the latest version from npmjs
 
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+    <main className="flex h-screen flex-col justify-center items-center text-center gap-12">
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className="text-4xl font-bold">Crease</h1>
+        <VersionTag version="v0.1.2" />
+      </div>
+      <div className='flex flex-col justify-center items-center'>
+        <h3 className='w-1/2 text-lg text-muted-foreground'>Crease is a responsive React component library powered by Tailwind CSS, designed to easily implement various types of interactive quizzes.</h3>
+        <Link href="/docs" className='z-10 my-4 flex items-center justify-center'>
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Read documentation
+            </span>
+          </ShimmerButton>
+        </Link>
+      </div>
     </main>
   );
 }
---
+++ app/components/VersionTag.tsx
@@ -0,0 +1,25 @@
+import AnimatedGradientText from "@/components/ui/animated-gradient-text";
+
+import { cn } from "@/lib/utils";
+
+type VersionTagProps = {
+  version: string,
+  className?: string,
+}
+
+export default function VersionTag({ version, className }: VersionTagProps) {
+  return (
+    <div className={`z-10 flex my-4 items-center justify-center ${className}`}>
+      <AnimatedGradientText>
+        Alpha <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
+        <span
+          className={cn(
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+          )}
+        >
+          { version }
+        </span>
+      </AnimatedGradientText>
+    </div>
+  );
+}
